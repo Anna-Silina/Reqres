5980fe55b8dee12e40112993a2f8cc6364b57ebb
Anna <sol11.06@gmail.com>
2022-05-23 00:49:57 +0300

    add

--- src/test/java/test/DeleteUserTest.java
+++ src/test/java/test/DeleteUserTest.java
@@ -1,26 +1,30 @@
 package test;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
 
+  public class UserDelete {
+        @Test
+        public void deleteUserTest() {
+            String body = given()
+                    .when()
+                    .delete("https://reqres.in/api/users/2")
+                    .then()
+                    .log().all()
+                    .statusCode(204)
+                    .extract().body().asString();
+            UserDelete userDelete = new Gson().fromJson(body, UserDelete.class);
+            int salaryTo = userDelete.getItems().get().getSalary().getTo();
+            System.out.println(salaryTo);
+            UserDelete userDeleteWithoutExpose = new GsonBuilder().excludeFieldsWithoutExposeAnnotation().create().fromJson(body, UserDelete.class);
+            System.out.println(userDeleteWithoutExpose);
+            System.out.println(userDelete);
 
 
-public class DeleteUserTest {
-    private class UserDelete{
-
+        }
     }
-    @Test
-    public void deleteUserTest() {
-        String body = given()
-                .when()
-                .delete("https://reqres.in/api/users/2")
-                .then()
-                .log().all()
-                .statusCode(204)
-                .extract().body().asString();
-        UserDelete userDelete = new Gson().fromJson(body,UserDelete.class);
 
-    }
-}
+
